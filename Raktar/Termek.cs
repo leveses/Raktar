c89a813f0b26253a1a7646e6c2fae23f531c2cde
leveses <boda.zsombor10@gmail.com>
2020-09-23 14:40:09 +0200

    Add project files.

---
+++ Raktar/Termek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raktar
+{
+    class Termek
+    {
+        private string kod;
+
+        public string KOD
+        {
+            get { return kod; }
+            set { kod = value; }
+        }
+        private string nev;
+
+        public string NEV
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+        private int ar;
+
+        public int AR
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+        private int db;
+
+        public int DB
+        {
+            get { return db; }
+            set { db = value; }
+        }
+
+
+
+    }
+}
